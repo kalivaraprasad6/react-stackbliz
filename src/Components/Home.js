d3c8495a6819ea403243228a36f51a9c67f4f49c
kalivaraprasad6 <kalivaraprasad71303@gmail.com>
2022-06-07 18:34:06 +0530

    1 file modified, 2 added, 2 deleted

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Carousel} from 'react-bootstrap'
-import Scenary1 from '../public/Images/Art.jpg'
+import '../Components/Images/Art.jpg';
 const Home = () => {
   return (
     <>
