88e14419602dfffc83c8ab164abeb63afd08dc5c
kalivaraprasad6 <kalivaraprasad71303@gmail.com>
2022-06-15 14:14:26 +0530

    2 files modified, 1 added

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,10 +1,46 @@
 import React from 'react';
-
+import './Contact.css';
 const Contact = () => {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <h1>Contact</h1>
-    </div>
+    <>
+      <div className="container">
+        <header className="text-center">
+          <h1 id="title">
+            <em> Contact Form</em>
+          </h1>
+          <p id="description">
+            <em>Thank you for your Intrest, Please fill the form</em>
+          </p>
+        </header>
+
+        <form action="#" id="contact-form">
+          <div className="form-group">
+            <label htmlFor="name" id="name-label">
+              <em> Name </em>
+            </label>
+            <input
+              type="text"
+              id="name-input"
+              className="form-control"
+              placeholder="Enter Name"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="" id="-label">
+              <em> Name </em>
+            </label>
+            <input
+              type="text"
+              id="name-input"
+              className="form-control"
+              placeholder="Enter Name"
+              required
+            />
+          </div>
+        </form>
+      </div>
+    </>
   );
 };
 export default Contact;
