a5cf87f80f77cba8d7b5718c572c414774f53f0c
kalivaraprasad6 <kalivaraprasad71303@gmail.com>
2022-06-30 17:47:27 +0530

    1 file modified, 1 added

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,6 +1,42 @@
 import React from 'react';
-
+import './Footer.css';
+import { Container, Row, Col } from 'react-bootstrap';
 const Footer = () => {
-  return <div>Footer</div>;
+  const footerData = [
+    {
+      item: 'Content',
+      class: 'footer_item',
+    },
+    {
+      item: 'Content',
+      class: 'footer_item',
+    },
+    {
+      item: 'Content',
+      class: 'footer_item',
+    },
+    {
+      item: 'Content',
+      class: 'footer_item',
+    },
+    {
+      item: 'Content',
+      class: 'footer_item',
+    },
+  ];
+  return (
+    <div>
+      <div className="footer">
+        <Container>
+          <Row>
+            <Col></Col>
+            <Col></Col>
+            <Col></Col>
+            <Col></Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
 };
 export default Footer;
