d9375e31f9488cee6299ba89f21f12bcaed7fd2c
kalivaraprasad6 <kalivaraprasad71303@gmail.com>
2022-07-11 12:11:09 +0530

    2 files modified, 1 added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,21 @@
 import React from 'react';
-
+import TextField from '@mui/material/TextField';
+import './Home.css';
 const Home = () => {
   return (
     <div style={{ textAlign: 'center' }}>
       <h1>Home</h1>
+      <div>
+        <p></p>
+        <TextField
+          className="txtfield"
+          id="outlined-basic"
+          label="Enter Name"
+          variant="outlined"
+          type="text"
+          inputProps={{ color: 'black' }}
+        />
+      </div>
     </div>
   );
 };
